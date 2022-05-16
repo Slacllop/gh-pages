338c74ec57a2b50b828f36a493b93eaf53875bc5
Slacllop <105606899+Slacllop@users.noreply.github.com>
2022-05-16 12:10:31 +0200

    Create contacto.php

---
+++ contactos/contacto.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Contactos</title>
+<link href="../css/contacto.css" rel="stylesheet" type="text/css" media="all"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Techno Contact Form Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<link rel="icon" href="../logo.png"> 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4940127678921067" crossorigin="anonymous"></script>
+<link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+</head>
+<body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<div class="wthree-dot">
+	<h1>EstufasParaTi</h1>
+	<div class="profile">
+		<div class="wrap">
+			<div class="contact">
+				<div class="contact-row agileits-w3layouts">  
+					<div class="contact-w3lsleft">
+						<div class="contact-grid agileits">
+							<h4>ESCRIBENOS</h4>
+							<form action="procesar_contactos.php" method="POST"> 
+								<input type="text" name="Nombre" placeholder="Nombre *" required="">
+								<input type="text" name="Apellidos" placeholder="Apellidos *" required=""> 
+								
+								<input type="email" name="Email" placeholder="Email *" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+								<!-- debe estar en el siguiente orden: caracteres @ caracteres. dominio (caracteres seguidos de un signo @, seguidos por más caracteres, y luego un "." -->
+								<textarea name="Mensaje" placeholder="Mensaje... *" required=""></textarea>
+								<input type="submit" value="Enviar" name="Enviar">
+							</form>	 
+						</div>
+					</div>
+					<div class="contact-w3lsright">
+						<div class="agileits-contact-right">
+							<h2 class="mt-3">Nuestros Contactos</h2>
+							<div class="address-row">
+								<div class="address-left">
+									<i class="fa fa-home" aria-hidden="true"></i>
+								</div>
+								<div class="address-right">
+									<h5>Visitanos</h5>
+									<p>Villanueva de la Cañada, Madrid</p>
+								</div>
+								<div class="clear"> </div>
+							</div>
+							<div class="address-row w3-agileits">
+								<div class="address-left">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<div class="address-right">
+									<h5>Mail</h5>
+									<p><a href="admin@estufasparati.es"> admin@estufasparati.es</a></p>
+								</div>
+								<div class="clear"> </div>
+							</div>
+							<div class="address-row">
+								<div class="address-left">
+									<i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+								</div>
+								<div class="address-right">
+									<h5>Llamanos</h5>
+									<p>+34 654368975</p>
+								</div>
+								<div class="clear"> </div>
+							</div> 
+						</div>
+					</div>
+					<div class="clear"> </div>
+				</div>	
+			</div> 
+			<div class="wthree_footer_copy">
+				<p>© Todos los derechos reservados</p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
